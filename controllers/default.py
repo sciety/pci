86b5cd1cb4d4e519f85f7bd1b70fce9dea81d707
LinaKind <l.kinduryte@elifesciences.org>
2026-01-08 12:10:57 +0200

    Wire up a view model property to the new view sciety/sciety#4140

--- controllers/default.py
+++ controllers/default.py
@@ -189,13 +189,7 @@ def test_content():
     response.view = "default/test_content.html"
 
     return dict(
-            pageTitle=getTitle("#HomeTitle"),
-            customText=getText("#HomeInfo"),
-            pageHelp=getHelp("#Home"),
-            shareable=True,
-            currentUrl=URL(c="default", f="test_content"),
-            pciRRactivated=pciRRactivated,
-            panel=None,
+            htmlSnippet="<b>Html snippet</b>",
         )
 
 
