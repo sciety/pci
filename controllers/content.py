6e73e36e619189c2b35e94272a0d2f7201389944
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-01-12 15:44:40 +0000

    Create a seam to handle more than one evaluation type sciety/sciety#4140

--- controllers/content.py
+++ controllers/content.py
@@ -3,13 +3,13 @@ from gluon.contrib.markdown import WIKI
 import re
 
 def _decode_evaluation_doi(path: str):
-    match = re.match(r'^(.*)\.rev(\d)(\d+)$', path)
+    match = re.match(r'^(.*)\.(rev|d|ar)(\d)(\d+)$', path)
     if not match:
         return None
-    recommendation_doi, round_number, evaluation_number = match.groups()
+    recommendation_doi, evaluation_type, round_number, evaluation_number = match.groups()
     return dict(
         recommendation_doi=recommendation_doi,
-        evaluation_type='rev',
+        evaluation_type=evaluation_type,
         round_number=round_number,
         evaluation_number=evaluation_number
     )
@@ -24,6 +24,11 @@ def doi():
 
     if decodedRequest is None:
         raise HTTP(400, "Invalid DOI")
+
+    if decodedRequest['evaluation_type'] == 'd':
+        raise HTTP(400, "Unsupported evaluation type")
+    if decodedRequest['evaluation_type'] == 'ar':
+        raise HTTP(400, "Unsupported evaluation type")
 
     reviewContentAsMarkdown = db.get_review_text(decodedRequest['recommendation_doi'], decodedRequest['round_number'], decodedRequest['evaluation_number'])
     if reviewContentAsMarkdown is None:
