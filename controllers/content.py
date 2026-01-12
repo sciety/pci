8ec67f227ed4246b29267d9cc9171e566fe00ec7
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-01-12 15:31:04 +0000

    Extract responsibility of decoding to a separate function sciety/sciety#4140

--- controllers/content.py
+++ controllers/content.py
@@ -1,23 +1,41 @@
 from gluon.http import HTTP # type: ignore
 from gluon.contrib.markdown import WIKI
 
-def doi():
-    path_param = '/'.join(request.args) if request.args else None
-    isAReview = "rev" in path_param
+def _decode_evaluation_doi(path: str):
+    isAReview = "rev" in path
     if not isAReview:
-        raise HTTP(404, "DOI is not for a review")
+        return None
 
     delimiter = ".rev"
-    delimiter_start_index = path_param.find(delimiter)
+    delimiter_start_index = path.find(delimiter)
     delimiter_stop_index = delimiter_start_index + len(delimiter)
-    recommendation_doi = path_param[:delimiter_start_index]
+    recommendation_doi = path[:delimiter_start_index]
     # We assume there aren't more than nine rounds of reviews
-    review_round_number = path_param[delimiter_stop_index:][0]
-    review_number = path_param[delimiter_stop_index:][1:]
-    reviewContentAsMarkdown = db.get_review_text(recommendation_doi, review_round_number, review_number)
+    review_round_number = path[delimiter_stop_index:][0]
+    review_number = path[delimiter_stop_index:][1:]
+
+    return dict(
+       recommendation_doi=recommendation_doi,
+       evaluation_type='rev',
+       round_number=review_round_number,
+       evaluation_number=review_number
+    )
+
+
+def doi():
+    if request.args is None:
+        return HTTP(400, "No DOI supplied")
+
+    path_param = '/'.join(request.args)
+    decodedRequest = _decode_evaluation_doi(path_param)
+
+    if decodedRequest is None:
+        raise HTTP(400, "Invalid DOI")
+
+    reviewContentAsMarkdown = db.get_review_text(decodedRequest['recommendation_doi'], decodedRequest['round_number'], decodedRequest['evaluation_number'])
     if reviewContentAsMarkdown is None:
         raise HTTP(404, "No such review")
-    
+
     reviewContentAsHtml = WIKI(reviewContentAsMarkdown, safe_mode="")
 
     return reviewContentAsHtml
