b37249ee3230c2c38d5977c04b24db71a8afcddd
David Moulton <d.moulton@elifesciences.org>
2026-01-13 11:51:43 +0000

    Refactor for better use of an Enum sciety/sciety#4140

--- controllers/content.py
+++ controllers/content.py
@@ -7,25 +7,6 @@ class EvaluationType(Enum):
     REVIEW = "rev"
     DECISION = "d"
     AUTHOR_RESPONSE = "ar"
-# >>> def handle_semaphore(light):
-# ...     match light:
-# ...         case Semaphore.RED:
-# ...             print("You must stop!")
-# ...         case Semaphore.YELLOW:
-# ...             print("Light will change to red, be careful!")
-# ...         case Semaphore.GREEN:
-# ...             print("You can continue!")
-# ...
-
-# >>> handle_semaphore(Semaphore.GREEN)
-# You can continue!
-
-# >>> handle_semaphore(Semaphore.YELLOW)
-# Light will change to red, be careful!
-
-# >>> handle_semaphore(Semaphore.RED)
-# You must stop!
-
 
 # We assume there are never more than nine review rounds
 def _decode_evaluation_doi(path: str):
@@ -35,20 +16,20 @@ def _decode_evaluation_doi(path: str):
     recommendation_doi, evaluation_type, round_number, evaluation_number = match.groups()
     return dict(
         recommendation_doi=recommendation_doi,
-        evaluation_type=evaluation_type,
+        evaluation_type=EvaluationType(evaluation_type),
         round_number=round_number,
         evaluation_number=evaluation_number
     )
 
 def _get_markdown_content_based_on_evaluation_type(decoded_request: str):
     match decoded_request['evaluation_type']:
-        case EvaluationType.DECISION.value:
+        case EvaluationType.DECISION:
             if decoded_request['evaluation_number'] != '':
                 raise HTTP(400, "Invalid DOI")
             return db.get_decision_text(decoded_request['recommendation_doi'], decoded_request['round_number'])
-        case EvaluationType.AUTHOR_RESPONSE.value:
+        case EvaluationType.AUTHOR_RESPONSE:
             raise HTTP(400, "Unsupported evaluation type")
-        case EvaluationType.REVIEW.value:
+        case EvaluationType.REVIEW:
             return db.get_review_text(decoded_request['recommendation_doi'], decoded_request['round_number'], decoded_request['evaluation_number'])
     return None
 
