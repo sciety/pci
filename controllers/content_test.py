6e47639995a5f2a64b0206b3e12d11568f4352ec
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-02-02 15:08:21 +0000

    Write a regression test sciety/sciety#4148

--- controllers/content_test.py
+++ controllers/content_test.py
@@ -1,14 +1,15 @@
 from typing import Iterator
 from unittest.mock import MagicMock, patch
+
 import pytest
+from gluon.http import HTTP  # type: ignore
 
 import controllers.content as controllers_module
 from controllers.content import (
     EvaluationType,
     _decode_evaluation_doi,
-    _get_markdown_content_based_on_evaluation_type
+    _get_markdown_content_based_on_evaluation_type,
 )
-
 
 test_cases = [
     {
@@ -50,9 +51,9 @@ test_cases = [
 ]
 
 
-@pytest.fixture(name='recommendation_mock')
+@pytest.fixture(name="recommendation_mock")
 def _recommendation_mock() -> Iterator[MagicMock]:
-    with patch.object(controllers_module, 'Recommendation') as mock:
+    with patch.object(controllers_module, "Recommendation") as mock:
         yield mock
 
 
@@ -68,12 +69,14 @@ class TestGetMarkdownContentBasedOnEvaluationType:
         recommendation_mock: MagicMock,
     ):
         recommendation_mock.get_by_doi.return_value = None
-        result = _get_markdown_content_based_on_evaluation_type({
-            "recommendation_doi": "10.1234/xyz",
-            "evaluation_type": EvaluationType.REVIEW,
-            "round_number": "1",
-            "evaluation_number": "2",
-        })
+        result = _get_markdown_content_based_on_evaluation_type(
+            {
+                "recommendation_doi": "10.1234/xyz",
+                "evaluation_type": EvaluationType.REVIEW,
+                "round_number": "1",
+                "evaluation_number": "2",
+            }
+        )
         recommendation_mock.get_by_doi.assert_called_once_with("10.1234/xyz")
         assert result is None
 
@@ -82,11 +85,28 @@ class TestGetMarkdownContentBasedOnEvaluationType:
         recommendation_mock: MagicMock,
     ):
         recommendation_mock.get_by_doi.return_value = []
-        result = _get_markdown_content_based_on_evaluation_type({
-            "recommendation_doi": "10.1234/xyz",
-            "evaluation_type": EvaluationType.REVIEW,
-            "round_number": "1",
-            "evaluation_number": "2",
-        })
+        result = _get_markdown_content_based_on_evaluation_type(
+            {
+                "recommendation_doi": "10.1234/xyz",
+                "evaluation_type": EvaluationType.REVIEW,
+                "round_number": "1",
+                "evaluation_number": "2",
+            }
+        )
         recommendation_mock.get_by_doi.assert_called_once_with("10.1234/xyz")
         assert result is None
+
+    def test_foo(
+        self,
+        recommendation_mock: MagicMock,
+    ):
+        recommendation_mock.get_by_doi.return_value = [{}]
+        with pytest.raises(HTTP):
+            _get_markdown_content_based_on_evaluation_type(
+                {
+                    "recommendation_doi": "10.1234/xyz",
+                    "evaluation_type": EvaluationType.DECISION,
+                    "round_number": "1",
+                    "evaluation_number": "3",
+                }
+            )
