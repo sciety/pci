b0e469b7d52889ae86edf662834fa73f05ede221
LinaKind <l.kinduryte@elifesciences.org>
2026-02-04 13:28:29 +0200

    Continue to roll out dataclass checks sciety/sciety#4148

--- controllers/content.py
+++ controllers/content.py
@@ -96,14 +96,13 @@ def _get_markdown_content_based_on_evaluation_type(decoded_request: NewDecodedRe
                 return None
             reviewRoundDecision = recommendations[decoded_request.round_number - 1]
             return reviewRoundDecision.recommendation_comments
-    match decoded_request.evaluation_type:
-        case EvaluationType.AUTHOR_RESPONSE:
-            if not decoded_request.round_number:
-                raise HTTP(400, "Invalid DOI due to missing round number")
+        case DecodedAuthorResponseRequest():
             if len(recommendations) < decoded_request.round_number:
                 return None
             reviewRoundDecision = recommendations[decoded_request.round_number - 1]
             return reviewRoundDecision.reply
+
+    match decoded_request.evaluation_type:
 
         case EvaluationType.REVIEW:
             if not decoded_request.round_number:
